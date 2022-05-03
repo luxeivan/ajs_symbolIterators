cf07b82b1efe718d873e507c3f2f8678aa9ee314
luxeivan <luxeivan@gmail.com>
2022-05-03 21:53:12 +0300

    second commit

--- src/js/__tests__/basic.test.js
+++ src/js/__tests__/basic.test.js
@@ -4,33 +4,33 @@ test('Team', () => {
   const team = new Team();
   console.log(team);
   const result = [];
-  for (let item of team) {
+  for (const item of team) {
     console.log(item);
     result.push(item);
   }
   const tobe = [
     {
-        name: 'Лучник',
-        type: 'Bowman',
-        health: 50,
-        level: 1,
-        attack: 40,
-        defence: 10
+      name: 'Лучник',
+      type: 'Bowman',
+      health: 50,
+      level: 1,
+      attack: 40,
+      defence: 10,
     }, {
-        name: 'Демон',
-        type: 'Daemon',
-        health: 50,
-        level: 1,
-        attack: 40,
-        defence: 10
+      name: 'Демон',
+      type: 'Daemon',
+      health: 50,
+      level: 1,
+      attack: 40,
+      defence: 10,
     }, {
-        name: 'Мечник',
-        type: 'Swordsman',
-        health: 50,
-        level: 1,
-        attack: 40,
-        defence: 10
-    }
-];
+      name: 'Мечник',
+      type: 'Swordsman',
+      health: 50,
+      level: 1,
+      attack: 40,
+      defence: 10,
+    },
+  ];
   expect(result).toEqual(tobe);
 });
--- src/js/team.js
+++ src/js/team.js
@@ -1,50 +1,51 @@
 export default class Team {
-    constructor() {
-        this.teams = [
-            {
-                name: 'Лучник',
-                type: 'Bowman',
-                health: 50,
-                level: 1,
-                attack: 40,
-                defence: 10
-            }, {
-                name: 'Демон',
-                type: 'Daemon',
-                health: 50,
-                level: 1,
-                attack: 40,
-                defence: 10
-            }, {
-                name: 'Мечник',
-                type: 'Swordsman',
-                health: 50,
-                level: 1,
-                attack: 40,
-                defence: 10
-            }
-        ];
+  constructor() {
+    this.teams = [
+      {
+        name: 'Лучник',
+        type: 'Bowman',
+        health: 50,
+        level: 1,
+        attack: 40,
+        defence: 10,
+      }, {
+        name: 'Демон',
+        type: 'Daemon',
+        health: 50,
+        level: 1,
+        attack: 40,
+        defence: 10,
+      }, {
+        name: 'Мечник',
+        type: 'Swordsman',
+        health: 50,
+        level: 1,
+        attack: 40,
+        defence: 10,
+      },
+    ];
+  }
+
+  [Symbol.iterator]() {
+    return this;
+  }
+
+  next() {
+    if (this.current === undefined) {
+      // инициализация состояния итерации
+      this.current = 0;
     }
-    [Symbol.iterator]() {
-        return this;
+    if (this.current < this.teams.length) {
+      this.current += 1;
+      return {
+        done: false,
+        value: this.teams[this.current - 1],
+      };
     }
-    next() {
-        if (this.current === undefined) {
-        // инициализация состояния итерации
-        this.current = 0;
-      }
-        if (this.current < this.teams.length) {
-            this.current++;
-            return {
-                done: false,
-                value: this.teams[this.current-1]
-            };
-        } else {
-            // очистка текущей итерации
-            delete this.current;
-            return {
-                done: true
-            };
-        }
-    }
+    // очистка текущей итерации
+    delete this.current;
+    return {
+      done: true,
+    };
+  }
 }
